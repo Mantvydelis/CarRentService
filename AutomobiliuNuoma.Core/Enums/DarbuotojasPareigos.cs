25e6ef57f2912a4e3fc17ff70e4492c1847d72b1
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-08-05 18:31:02 +0300

    Pridetas Darbuotojo Model

---
+++ AutomobiliuNuoma.Core/Enums/DarbuotojasPareigos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobiliuNuoma.Core.Enums
+{
+    public enum DarbuotojasPareigos
+    {
+        Vadybininkas = 2,
+        Direktorius = 1,
+        Mechanikas = 3
+
+    }
+}
---
+++ AutomobiliuNuoma.Core/Models/Darbuotojas.cs
@@ -0,0 +1,25 @@
+﻿using AutomobiliuNuoma.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobiliuNuoma.Core.Models
+{
+    public class Darbuotojas
+    {
+        public int Id { get; set; }
+        public string Vardas { get; set; }
+        public string Pavarde { get; set; }
+        public DarbuotojasPareigos Pareigos { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {Vardas} {Pavarde} Pareigos: {Pareigos}";
+        }
+
+
+
+    }
+}
